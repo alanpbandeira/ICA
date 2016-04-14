790f35f48b83179668c5d10b74f391fb461c677f
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-04-14 16:09:50 -0300

    fixed places

---
+++ HillClibing/hillClibing.py
@@ -0,0 +1,42 @@
+from problem import Problem
+import random
+
+class HillClibing:
+	"""docstring for HillClibing"""
+	
+	def __init__(self, max_iterations, problem):
+		self.max_iterations = max_iterations
+		self.problem = problem
+
+	# Nota: limitar o pertubador para o limitante
+	@staticmethod
+	def randomPertubator(value, maximize):
+		if maximize is True:
+			return value + random.random()
+		else:
+			return value - random.random()
+
+	def run(self):
+		maximization = self.problem.maximization
+		candidate_value = random.uniform(self.problem.interval[0], self.problem.interval[1])
+		candidate_score = self.problem.setScore(candidate_value)
+			
+		best_result = candidate_value
+		best_score  = candidate_score
+		improvement = True
+		iterations  = 1
+		
+		while((iterations <= self.max_iterations) and improvement):
+
+			candidate_value = self.randomPertubator(candidate_value, maximization)
+			candidate_score = self.problem.setScore(candidate_value)
+
+			best_result = self.problem.evaluate(candidate_value, best_result)
+
+			if best_result != candidate_value:
+				improvement = False
+
+			iterations += 1
+
+		return (iterations, best_result, self.problem.setScore(best_result))
+		
---
+++ HillClibing/problem.py
@@ -0,0 +1,33 @@
+import math
+
+class Problem():
+	"""docstring for Problem"""
+
+	def __init__(self, maximization, interval):
+		self.maximization = maximization
+		self.interval = interval
+
+	@staticmethod
+	def objective_function(x):
+		element_one = -2 * math.pow(((x - 0.1) / 0.9), 2)
+		element_two = math.pow(math.sin(5 * math.pi * x), 6)
+		result = 2 * (math.exp(element_one * element_two))
+		return result
+
+	def setScore(self, value):
+		return self.objective_function(value)
+
+	def evaluate(self, current_value, candidate_value):
+		candidate_score = self.setScore(candidate_value)
+		current_score   = self.setScore(current_value)
+
+		if self.maximization:
+			if (candidate_score > current_score):
+				return candidate_value
+			else:
+				return current_value
+		else:
+			if (candidate_score < current_score):
+				return candidate_value
+			else:
+				return current_value
