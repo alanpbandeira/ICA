59fb08800c90072337a28d2be049cbfde29b1503
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-10-21 18:59:51 -0200

    minor changes2

--- App/ANN/neural_module/neuronset_model.py
+++ App/ANN/neural_module/neuronset_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from .neuron_model import Neuron
+from ...ANN.MathLib.matrix_module import euclidianDist
 
 
 class NeuronLayer(object):
@@ -21,6 +22,8 @@ class NeuronLayer(object):
 		self._radius = n_radius
 		self._layer_dim = dimensions
 		self._size = _layer_dim[0] * _layer_dim[1]
+
+		self._neurons = self.neuronsFromData(data_set)
 		self._neuron_layer = None
 
 	def __len__(self):
@@ -57,12 +60,9 @@ class NeuronLayer(object):
 					continue
 				else:
 					candidates.append(candidate)
-					count += 1		
-		
-		return [Neuron(candidate.data) for candidate in candidates]
+					count += 1
 
-	def setNeighbours(self):
-		pass
+		return [Neuron(candidate.data) for candidate in candidates]
 
 	def genLayerIndexes(self):
 		"""
@@ -86,17 +86,41 @@ class NeuronLayer(object):
 
 		return indexes
 
+	def setNeighbours(self):
+		"""
+		"""
+
+		for neuron in self._neurons:
+			candidates = []
+
+			for candidate in self._neurons:
+				if candidate is neuron:
+					continue
+
+				p_one = np.array(neuron.index)
+				p_two = np.array(candidate.index)
+				if euclidianDist(p_one, p_two) <= self._radius:
+					candidates.append(candidate.index)
+
 	def buildLayer(self, data_set):
 		"""
 		"""
 
-		l_neurons = self.neuronsFromData(data_set)
-		l_indexes = self.genLayerIndexes()
+		layer_idx = self.genLayerIndexes()
 
-		for neuron in l_neurons:
+		class_ids = range(1, (len(l_neurons) + 1))
+
+		for idx, c_id in zip(layer_idx, class_ids):
+			elected_neu = np.random.choice(range(self._size))
+			self._neurons[elected_neu].index(idx)
+			self._neurons[elected_neu].class_id(c_id)
+
+		# set neighbours !TO DO!
+
+
+
 
 
 #
 #	PROPERTIES
 #
-
